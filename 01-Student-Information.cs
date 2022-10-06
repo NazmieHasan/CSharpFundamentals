3544df6eb915bc76fa2c1fd2ad275694b248dbba
Nazmie Hasan <Nazi87@abv.bg>
2022-10-06 23:43:26 +0300

    Initial commit

---
+++ 01-Student-Information.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+        
+    }
+}
