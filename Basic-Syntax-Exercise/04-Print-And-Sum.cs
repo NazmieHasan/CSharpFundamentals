1dfbd19b1d33236a167fbeb23609ae15edbebb19
Nazmie Hasan <Nazi87@abv.bg>
2022-11-14 16:03:09 +0200

    add solutions in Basic Syntax

---
+++ Basic-Syntax-Exercise/04-Print-And-Sum.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main()
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            for (int i = startNum; i <= endNum; i++) {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ Basic-Syntax-Lab/10-Multiplication-Table.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int times = 1;
+           
+            while (times <= 10) 
+            {
+                Console.WriteLine($"{number} X {times} = {number * times}");
+                times++;
+            }
+           
+        }
+    }
+}
